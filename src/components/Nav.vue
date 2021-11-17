96bdc3d3f55df578111e5a51e8930594966253c6
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-16 22:57:39 -0600

    UPDATE2

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="text-red-400 bg-indigo-600">
     <div class="container flex items-center justify-center mx-auto">
-      <h1 class="text-4xl font-medium tracking-tight">
+      <router-link to="/">
+      <h1 class="text-4xl font-medium tracking-tight py-9">
         <span class="font-normal">Advanced API</span>
       </h1>
-      
+      </router-link>
     </div>
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,31 @@
 <script setup>
-import { ref } from "vue";
-import { useAdvice } from "../composables/pushapi";
-const { advices, search } = useAdvice();
-const searchItem = ref("");
+
+import {useAPI} from "../composables/useAPI";
+
+const {movies, getMovies} = useAPI();
+
 </script>
 
 <template>
-  <div>
-    <input
-      v-model="searchItem"
-      @change="search(searchItem)"
-      type="text"
-      placeholder="Search an Advise..."
-      class="w-full py-4 mt-16 text-xl text-center rounded-full"
-    />
-    <div class="grid grid-cols-2 gap-16 pt-16">
-      <div v-for="(advice, index) in advices" :key="index">
-        <p>
-          {{ advice.advice }}
-        </p>
+ 
+
+<div class="mt-18">
+  <ul class="grid grid-cols-4 gap-10">
+    <router-link class="p-3 bg-gray-400 rounded-md shadow-xl cursor-pointer hover:scale-125" v-for="movie in movies" :key="movies._id" :to="'/movie/${movie._id}'">
+      <div>
+        <h3 class="text-lg font-semibold">{{movie.name}}</h3>
+        <p>Runtime:  {{movie.runtimeInMinutes}} Minutes</p>
+        <p>Budget: ${{movie.budgetInMillions}} Million</p>
+        <p>Revenue: ${{movie.boxOfficeRevenueInMillions}}</p>
+        <p>Academy Wins: {{movie.academyAwardWins}}</p>
       </div>
-    </div>
-  </div>
+    </router-link>
+      
+
+   
+  </ul>
+
+
+</div>
+
 </template>
