e142a130034f265fe17d2dfbc7fece5f80952be3
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-16 21:03:47 -0600

    basic changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <div class="text-red-400 bg-indigo-600">
     <div class="container flex items-center justify-center mx-auto">
       <h1 class="text-4xl font-medium tracking-tight">
-        <span class="font-normal">API</span>
+        <span class="font-normal">Advanced API</span>
       </h1>
       
     </div>
