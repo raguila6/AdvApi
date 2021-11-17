1acd403bfa3d07bb2afb1ffb6f6d5876d28e0966
raguila6 <92766683+raguila6@users.noreply.github.com>
2021-11-16 20:56:00 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-red-400 bg-indigo-600">
+    <div class="container flex items-center justify-center mx-auto">
+      <h1 class="text-4xl font-medium tracking-tight">
+        <span class="font-normal">API</span>
+      </h1>
+      
+    </div>
+  </div>
+</template>
